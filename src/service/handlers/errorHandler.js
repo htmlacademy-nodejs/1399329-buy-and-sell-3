85f69797e53c0cfdaec09b404e8f81ad165a5ce7
behemothoz <behemothoz@yandex.ru>
2020-05-11 17:14:30 +0300

    express for api server

---
+++ src/service/handlers/errorHandler.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const {HttpCode} = require(`../../constants`);
+const log = require(`../cli/console`);
+
+const INTERNAL_SERVER_MSG_TEXT = `Something went wrong`;
+
+const errorHandler = (err, _, res) => {
+  log.error(err);
+  res.status(HttpCode.INTERNAL_SERVER_ERROR).send(INTERNAL_SERVER_MSG_TEXT);
+};
+
+module.exports = errorHandler;
