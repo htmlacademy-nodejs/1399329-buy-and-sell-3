3cc7436b135bca9bd7930480ae5df0a28ede31f3
behemothoz <behemothoz@yandex.ru>
2020-05-11 21:36:10 +0300

    fix eslint errors

--- src/service/handlers/errorHandler.js
+++ src/service/handlers/errorHandler.js
@@ -5,6 +5,7 @@ const log = require(`../cli/console`);
 
 const INTERNAL_SERVER_MSG_TEXT = `Something went wrong`;
 
+// eslint-disable-next-line  no-unused-vars
 const errorHandler = (err, req, res, next) => {
   log.error(err, err.stack);
   res.status(HttpCode.INTERNAL_SERVER_ERROR).send(INTERNAL_SERVER_MSG_TEXT);
